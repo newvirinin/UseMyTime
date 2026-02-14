183fd4ec0ed9d4d7f315e38172e5c856b2fed9cc
Semyon Averin <semyonaverin@MacBook-Pro-Semyon.local>
2026-02-14 12:24:37 +0700

    Fix wsgi.py settings module path

--- UseMyTime/wsgi.py
+++ UseMyTime/wsgi.py
@@ -12,9 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Force the settings module to override any environment variable
-os.environ['DJANGO_SETTINGS_MODULE'] = 'UseMyTime.settings'
-
-# Debug print to verify the settings module (will appear in logs)
-print(f"DJANGO_SETTINGS_MODULE is set to: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
